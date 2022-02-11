551c31535ac2b6ca6544a85c2a56643677ff203a
Leonardo <leonardomibsp@hotmail.com>
2022-02-11 10:01:55 -0300

    connect firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,18 +1,14 @@
-import firebase from 'firebase/app'
-import { initializeApp } from "firebase/app"
+import { initializeApp } from "firebase/app";
 
-import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5wtXgUJeF91paYj0pMBXWecNuD0F3i9A",
-  authDomain: "let-me-ask-b8622.firebaseapp.com",
-  databaseURL: "https://let-me-ask-b8622-default-rtdb.firebaseio.com",
-  projectId: "let-me-ask-b8622",
-  storageBucket: "let-me-ask-b8622.appspot.com",
-  messagingSenderId: "996929989125",
-  appId: "1:996929989125:web:383824f64fde7bb5dbca6c"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 }
 
-firebase.initializeApp(firebaseConfig)
-
-const auth = firebase.auth()
+const app = initializeApp(firebaseConfig);
